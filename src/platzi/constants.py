212f0a2e821453f124981c42a819098f70a686ac
alpha-drm <alpha_drm@outlook.com>
2025-11-12 01:19:48 -0300

    refactor: add random USER_AGENTS

--- src/platzi/constants.py
+++ src/platzi/constants.py
@@ -1,3 +1,4 @@
+import random
 from pathlib import Path
 
 import platformdirs
@@ -13,18 +14,21 @@ LOGIN_DETAILS_URL = "https://api.platzi.com/api/v1/components/headerv2/user/"
 PLATZI_URL = "https://platzi.com"
 REFERER = "https://platzi.com/"
 
-USER_AGENT = (
-    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
-    "AppleWebKit/537.36 (KHTML, like Gecko) "
-    "Chrome/121.0.0.0 Safari/537.36"
-)
+USER_AGENTS = [
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:144.0) Gecko/20100101 Firefox/144.0",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/142.0.0.0 Safari/537.36",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 14_4_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36 OPR/109.0.0.0",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/142.0.0.0 Safari/537.36 Edg/142.0.0.0",
+]
 
 HEADERS = {
-    "User-Agent": USER_AGENT,
+    "User-Agent": random.choice(USER_AGENTS),
     "Referer": REFERER,
     "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
-    "Accept-Language": "es-ES,es;q=0.9,en;q=0.8",
+    "Accept-Language": "es-ES,es;q=0.8,en-US;q=0.5,en;q=0.3",
     "Connection": "keep-alive",
+    "Host": "mediastream.platzi.com",
+    "Origin": "https://platzi.com",
 }
 
 
